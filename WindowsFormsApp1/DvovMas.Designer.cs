37943cf92f70912a78bdf2d585f85b865e26ba86
Timur Komusar <komistimur91@gmail.com>
2023-11-29 02:56:48 +0200

    pr2

--- WindowsFormsApp1/DvovMas.Designer.cs
+++ WindowsFormsApp1/DvovMas.Designer.cs
@@ -29,32 +29,160 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Xget = new System.Windows.Forms.TextBox();
+            this.Yget = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(415, 304);
+            this.button1.Location = new System.Drawing.Point(575, 402);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 46);
+            this.button1.Size = new System.Drawing.Size(117, 36);
             this.button1.TabIndex = 0;
             this.button1.Text = "Закрити";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(450, 426);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(575, 361);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Розрахувати";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(564, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(564, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Y";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(535, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Найбільше значення:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(572, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Позиція -";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(686, 161);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(644, 198);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 16);
+            this.label6.TabIndex = 8;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Xget
+            // 
+            this.Xget.Location = new System.Drawing.Point(614, 81);
+            this.Xget.Name = "Xget";
+            this.Xget.Size = new System.Drawing.Size(100, 22);
+            this.Xget.TabIndex = 9;
+            this.Xget.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Yget
+            // 
+            this.Yget.Location = new System.Drawing.Point(614, 119);
+            this.Yget.Name = "Yget";
+            this.Yget.Size = new System.Drawing.Size(100, 22);
+            this.Yget.TabIndex = 10;
+            this.Yget.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // DvovMas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Yget);
+            this.Controls.Add(this.Xget);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Name = "DvovMas";
             this.Text = "DvovMas";
+            this.Load += new System.EventHandler(this.DvovMas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Xget;
+        private System.Windows.Forms.TextBox Yget;
     }
 }
--- WindowsFormsApp1/DvovMas.cs
+++ WindowsFormsApp1/DvovMas.cs
@@ -23,5 +23,94 @@ namespace WindowsFormsApp1
             form.Show();
             this.Close();
         }
+
+        private void DvovMas_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n = 0, m = 0;
+
+                n = Convert.ToInt32(Xget.Text);
+                m = Convert.ToInt32(Yget.Text);
+
+                if (n > 5 || m > 5)
+                    throw new Exception("Масив більший за 5/5");
+
+                Arrays2D b = new Arrays2D(n, m);
+                DataGridViewTextBoxColumn dvage;
+
+                for(int i =0; i<m; i++)
+                {
+                    dvage = new DataGridViewTextBoxColumn();
+                    dvage.Width = 40;
+                    dataGridView1.Columns.Add(dvage);
+                }
+
+                dataGridView1.Rows.Clear();
+                dataGridView1.RowCount = b.X_l;
+                dataGridView1.ColumnCount = b.Y_l;
+
+                for (int i = 0; i < b.X_l; i++)
+                    for (int j = 0; j < b.Y_l; j++)
+                    {
+                        dataGridView1.Rows[i].Cells[j].Value = b[i, j].ToString();
+                    }
+
+                var r = b.Nayb_Vidn_LivDiag;
+                label5.Text = r.Item1.ToString();
+                label6.Text = r.Item2.ToString();
+
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
     }
 }
